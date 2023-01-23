c7658ece962ee03cfb6daa8b2fcd53b5788af8cd
OG <103863031+RyzKum@users.noreply.github.com>
2023-01-23 15:49:40 +0100

    barboj

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+    public static void main(String[] args) {
+		
+		//alignement requis :
+		int N = 4;
+		//colonnes et lignes :
+		int C = 7;
+		int L = 6;
+		//tableau du plateau ('.' = emplacement vide  / 'X' = joueur 1  /  'O'  = joueur 2)
+		char[][] plateau = new char[C][L];
+		//remplissage des cases avec du vide, oui c'est paradoxal !
+		for(int x = 0 ; x < C ; x++)
+			for(int y = 0 ; y < L ; y++)
+				plateau[x][y] = '.';
+	
+			
+			for(int loop = 0 ; loop < C+2+2*C ; loop++)System.out.print('-');
+			System.out.println();
+			
+			for(int y = 0 ; y < L ; y++){
+				System.out.print('|');
+				for(int x = 0 ; x < C ; x++){
+					System.out.print(" " + plateau[x][y] + " ");
+				}
+				System.out.print('|');
+				System.out.println();
+			}
+            for(int loop = 0 ; loop < C+2+2*C ; loop++)System.out.print('-');
+			System.out.println();
+}}
---
+++ src/Tableau.java
@@ -0,0 +1,21 @@
+public class Tableau {
+    public static void main(String[] args) throws Exception {
+        // déclaration du tableau qui représente la grille de Puissance 4
+         int[][] grille = new int[6][7];
+ 
+         // initialisation de la grille avec des valeurs de 0 (aucune pièce jouée)
+         for (int i = 0; i < 6; i++) {
+             for (int j = 0; j < 7; j++) {
+                 grille[i][j] = 0;
+             }
+         }
+ 
+         // affichage de la grille
+         for (int i = 0; i < 6; i++) {
+             for (int j = 0; j < 7; j++) {
+                 System.out.print(grille[i][j] + " ");
+             }
+             System.out.println();
+         }
+     }
+}
